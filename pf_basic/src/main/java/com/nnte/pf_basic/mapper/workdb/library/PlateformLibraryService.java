4fd5854929aa7569a060f76d44c9c7cb6c6ca2f1
liyihehai <liyi_hehai@sina.com>
2022-04-07 22:38:11 +0800

    '自动代码提交'

---
+++ pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/library/PlateformLibraryService.java
@@ -0,0 +1,12 @@
+package com.nnte.pf_basic.mapper.workdb.library;
+
+import com.nnte.framework.base.BaseService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlateformLibraryService extends BaseService<PlateformLibraryDao,PlateformLibrary> {
+    public PlateformLibraryService(){
+        super(PlateformLibraryDao.class);
+    }
+}
+
