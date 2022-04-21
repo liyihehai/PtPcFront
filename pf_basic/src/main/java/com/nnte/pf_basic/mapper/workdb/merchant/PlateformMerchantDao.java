25bc62ab7b0bc177bb076139dc73b85b2e5b347b
liyihehai <liyi_hehai@sina.com>
2022-04-21 09:50:50 +0800

    删除错误的文件生成

--- pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/merchant/PlateformMerchantDao.java
+++
@@ -1,7 +0,0 @@
-package com.nnte.pf_basic.mapper.workdb.merchant;
-
-import com.nnte.framework.base.BaseDao;
-
-public interface PlateformMerchantDao extends BaseDao {
-
-}
--- pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/merchant_expand/PlateformMerchantExpandDao.java
+++
@@ -1,7 +0,0 @@
-package com.nnte.pf_basic.mapper.workdb.merchant_expand;
-
-import com.nnte.framework.base.BaseDao;
-
-public interface PlateformMerchantExpandDao extends BaseDao {
-
-}
