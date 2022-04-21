513ef024d8dc8968a2e0c1b4b1334b98394708ea
liyihehai <liyi_hehai@sina.com>
2022-04-21 09:54:47 +0800

    '自动代码提交'

--- pf_merchant/src/main/java/com/nnte/pf_merchant/mapper/workdb/merchant/PlateformMerchant.java
+++ pf_merchant/src/main/java/com/nnte/pf_merchant/mapper/workdb/merchant/PlateformMerchant.java
@@ -3,8 +3,8 @@ import com.nnte.framework.base.BaseModel;
 import com.nnte.framework.annotation.DBPKColum;
 
 import java.util.Date;
-/*
- * 自动代码 请勿更改 <2022-03-28 13:16:12>
+/**
+ * auto code,do not modify <2022-04-21 09:54:47>
  */
 public class PlateformMerchant extends BaseModel {
     @DBPKColum private Integer id;
@@ -12,9 +12,13 @@ public class PlateformMerchant extends BaseModel {
     private String pmName;
     private String pmShortName;
     private Integer pmCompanyPerson;
+    private String pmLogo;
     private Integer pmState;
     private Date createTime;
     private String applyEmail;
+    private String createBy;
+    private String updateBy;
+    private Date updateTime;
 
     public PlateformMerchant(){}
 
@@ -28,10 +32,18 @@ public class PlateformMerchant extends BaseModel {
     public void setPmShortName(String  pmShortName){ this.pmShortName = pmShortName;}
     public Integer  getPmCompanyPerson(){ return pmCompanyPerson;}
     public void setPmCompanyPerson(Integer  pmCompanyPerson){ this.pmCompanyPerson = pmCompanyPerson;}
+    public String  getPmLogo(){ return pmLogo;}
+    public void setPmLogo(String  pmLogo){ this.pmLogo = pmLogo;}
     public Integer  getPmState(){ return pmState;}
     public void setPmState(Integer  pmState){ this.pmState = pmState;}
     public Date  getCreateTime(){ return createTime;}
     public void setCreateTime(Date  createTime){ this.createTime = createTime;}
     public String  getApplyEmail(){ return applyEmail;}
     public void setApplyEmail(String  applyEmail){ this.applyEmail = applyEmail;}
+    public String  getCreateBy(){ return createBy;}
+    public void setCreateBy(String  createBy){ this.createBy = createBy;}
+    public String  getUpdateBy(){ return updateBy;}
+    public void setUpdateBy(String  updateBy){ this.updateBy = updateBy;}
+    public Date  getUpdateTime(){ return updateTime;}
+    public void setUpdateTime(Date  updateTime){ this.updateTime = updateTime;}
 }
