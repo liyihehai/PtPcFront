58ed6f3c5fa108f6d9c6971cb6b2e68db368634f
liyihehai <liyi_hehai@sina.com>
2022-04-21 09:46:17 +0800

    '自动代码提交'

---
+++ pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/merchant_expand/PlateformMerchantExpandService.java
@@ -0,0 +1,12 @@
+package com.nnte.pf_basic.mapper.workdb.merchant_expand;
+
+import com.nnte.framework.base.BaseService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlateformMerchantExpandService extends BaseService<PlateformMerchantExpandDao,PlateformMerchantExpand> {
+    public PlateformMerchantExpandService(){
+        super(PlateformMerchantExpandDao.class);
+    }
+}
+
