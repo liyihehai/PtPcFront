95af295e81fe0d5ec6b6e69d86233a28c648ee59
liyihehai <liyi_hehai@sina.com>
2022-05-16 13:44:48 +0800

    '自动代码提交'

---
+++ pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/utiAccount/PlateformMerchantUtiAccountDao.java
@@ -0,0 +1,7 @@
+package com.nnte.pf_basic.mapper.workdb.utiAccount;
+
+import com.nnte.framework.base.BaseDao;
+
+public interface PlateformMerchantUtiAccountDao extends BaseDao {
+
+}
---
+++ pf_basic/src/main/java/com/nnte/pf_basic/mapper/workdb/utiAccount/PlateformMerchantUtiAccountService.java
@@ -0,0 +1,12 @@
+package com.nnte.pf_basic.mapper.workdb.utiAccount;
+
+import com.nnte.framework.base.BaseService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlateformMerchantUtiAccountService extends BaseService<PlateformMerchantUtiAccountDao,PlateformMerchantUtiAccount> {
+    public PlateformMerchantUtiAccountService(){
+        super(PlateformMerchantUtiAccountDao.class);
+    }
+}
+
